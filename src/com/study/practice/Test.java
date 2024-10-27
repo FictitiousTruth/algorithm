612ef2c254619d92b45ea09d35f42ab893df8624
Lnovo <2205186558@qq.com>
2024-10-27 15:29:12 +0800

    2345

--- src/com/study/practice/Test.java
+++ src/com/study/practice/Test.java
@@ -2,9 +2,7 @@ package com.study.practice;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("版本2");
-        System.out.println("resert");
-
+        System.out.println(2345);
 
     }
 
