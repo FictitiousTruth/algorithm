d01969a3651820fbbfd7b3ac0f010248175ee1f1
Lnovo <2205186558@qq.com>
2024-10-27 14:37:03 +0800

    测试commit2

--- src/com/study/practice/test/Test.java
+++ src/com/study/practice/test/Test.java
@@ -3,5 +3,6 @@ package com.study.practice.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("测试commit");
+        System.out.println("测试commit2");
     }
 }
